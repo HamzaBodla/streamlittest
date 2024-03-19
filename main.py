46986978a7eab29a7049879461657163cd8e409f
HamzaBodla <hamzabodla97@gmail.com>
2024-03-19 13:59:13 +0000

    ice server added

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ def video_frame_callback(frame):
     return av.VideoFrame.from_ndarray( format="bgr24")
 
 
-webrtc_streamer(key="example", video_frame_callback=video_frame_callback)
+webrtc_streamer(key="example", video_frame_callback=video_frame_callback, rtc_configuration={"iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
